dc33b0bff9231df9c8d4622bb19094d238da011c
RayhanalKavey <rayhanalkavey@gmail.com>
2023-03-14 22:05:57 +0600

    Redux toolkit initial setup.

--- src/pages/home/Home/Home.js
+++ src/pages/home/Home/Home.js
@@ -1,9 +1,15 @@
 import React from "react";
+import Spinner from "../../../components/Spinner/Spinner";
 import useTitle from "../../../hooks/useTitle/useTitle";
 
 const Home = () => {
   useTitle("Home");
-  return <div>This is home page</div>;
+  return (
+    <div>
+      This is home page
+      <Spinner></Spinner>
+    </div>
+  );
 };
 
 export default Home;
