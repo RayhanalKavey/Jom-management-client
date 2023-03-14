ead5b0600f57a1922390e9b4d886d8292a2598f2
RayhanalKavey <rayhanalkavey@gmail.com>
2023-03-14 16:08:32 +0600

    Add tailwind css and dark mode system with system preference.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [theme, setTheme] = useState(null);
+
+  // Check the system preference. It will check this once.
+  useEffect(() => {
+    window.matchMedia("prefers-color-scheme:dark").matches
+      ? setTheme("dark")
+      : setTheme("light");
+  }, []);
+
+  // Set the theme according the handleThemeSwitch functions theme preference
+  useEffect(() => {
+    theme === "dark"
+      ? document.documentElement.classList.add("dark")
+      : document.documentElement.classList.remove("dark");
+  }, [theme]);
+
+  // Switch theme function called when user want to switch theme
+  const handleThemeSwitch = () => {
+    setTheme(theme === "dark" ? "light" : "dark");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className=" bg-green-500 dark:bg-slate-500">
+      <button className=" bg-green-300" onClick={handleThemeSwitch}>
+        Theme switcher
+      </button>
     </div>
   );
 }
